12ad4463b2fba49603effb211087fbe966f1b6fe
WilliamKropog <william.kropog@outlook.com>
2026-08-11 14:10:33 -0500

    Start button in Navbar component.

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, MatButton],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar {}
+export class NavBar {
+  private readonly router = inject(Router);
+
+  protected onStartGame(): void {
+    void this.router.navigate(['/game']);
+  }
+}
