5716239a10ee779d8a00fd6b23e9073b3d8ffcd5
WilliamKropog <william.kropog@outlook.com>
2026-08-11 14:07:36 -0500

    Added Home Page template.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { DeckBuilderPage } from './deck-builder-page/deck-builder-page';
+import { GamePage } from './game-page/game-page';
+import { HomePage } from './home-page/home-page';
+import { StorePage } from './store-page/store-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomePage },
+  { path: 'game', component: GamePage },
+  { path: 'store', component: StorePage },
+  { path: 'deck-builder', component: DeckBuilderPage },
+  { path: '**', redirectTo: '' },
+];
--- src/app/home-page/home-page.ts
+++ src/app/home-page/home-page.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { Router } from '@angular/router';
+import { NavBar } from '../nav-bar/nav-bar';
+import { SideNav } from '../side-nav/side-nav';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [NavBar, SideNav, MatButton],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
 export class HomePage {
+  private readonly router = inject(Router);
 
+  protected onStartGame(): void {
+    void this.router.navigate(['/game']);
+  }
 }
